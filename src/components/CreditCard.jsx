e1898046e2a79a683c98186c20f0d2741b4025c8
aleixnit <nitjac210@gmail.com>
2023-05-23 14:01:57 +0200

    number card dinamico

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -4,11 +4,16 @@ import VisaCard from '../assets/images/visa.png'
 import MasterCard from '../assets/images/MasterCard.png'
 
 export default function CreditCard(props) {
+     // Obtener los últimos cuatro dígitos del número de tarjeta
+  const lastFourDigits = props.number.slice(-4);
+  
+  // Reemplazar los dígitos restantes por el símbolo "·"
+  const hiddenNumber = props.number.slice(0, -4).replace(/[0-9]/g, '·').match(/.{1,4}/g).join(' ');
 
  return (
     <div style={{backgroundColor: props.bgColor, color: props.color, margin: '0 auto'}} className= "container">
         <img src={props.type == 'Visa' ? VisaCard : MasterCard} alt="imagen" />
-        <p className="numberCard">{props.number}</p>
+        <p className="numberCard">{hiddenNumber + ' ' + lastFourDigits}</p>
         <div style={{display: 'flex', justifyContent: 'flex-start', flexDirection: 'column', marginLeft: '20px'}}>
             <div style={{display: 'flex', gap: '20px'}}>
                 <p>{props.expirationMonth}/{props.expirationYear}</p>
