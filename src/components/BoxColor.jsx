160894d0fb7737000d6e0d6b869c691eb12e096c
aleixnit <nitjac210@gmail.com>
2023-05-23 18:43:13 +0200

    HexColor conseguido

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,13 +1,26 @@
 import './Greetings.css'
 
 export default function BoxColor(props) {
-
     let colorRGB = `rgb(${props.r}, ${props.g}, ${props.b})`;
 
+
+    function ColorToHex(color) {
+        let hexadecimal = color.toString(16);
+        return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+      }
+      
+      function ConvertRGBtoHex(red,green,blue) {
+
+        return "#" + ColorToHex(props.r) + ColorToHex(props.g) + ColorToHex(props.b);
+      }
+
+      const hexValue = ConvertRGBtoHex(props.r, props.g, props.b);
+    //   console.log(ConvertRGBtoHex(255, 100, 200));
+
  return (
-    <div style={{backgroundColor: colorRGB}} className="textBoxColor">
-        <p>rgb({props.r}, {props.g}, {props.b})</p>
-        <p>#ff0000</p>
+    <div style={{backgroundColor: colorRGB, display: 'flex', flexDirection: 'column'}} className="textBoxColor">
+        <p style={{margin: '0'}}>rgb({props.r}, {props.g}, {props.b})</p>
+        <p style={{margin: '0'}}>{hexValue}</p>
     </div>
  )
 }
